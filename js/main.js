b34ce41fddace68949c3ab800bf67942c80f05fb
Andrew Vernier <amverni@umich.edu>
2019-09-02 13:24:24 -0400

    Switched navigation to have dropdowns

--- js/main.js
+++ js/main.js
@@ -3,72 +3,89 @@ var originalHomeColorOpacity = 0.7; /* Must match value in style.css #home */
 
 $(document).ready(function()
 {
-    originalPaddingHeight = parseFloat($('#header').css('padding-bottom'));
-    set_scrollspy_offset();
+    originalPaddingHeight = parseFloat($('#header').css('padding-top'));
+
     fade_in_elements(['.fade-in-1', '.fade-in-2']);
 
-    set_scrollspy_offset();
-    $('#navbar-btn').click(set_scrollspy_offset);
-    $('.scroll').click(function(event) { scroll_to_link(event, $($(this).attr('href')).offset().top); });
+    $('.scroll').click( function(event)
+    {
+        var page, element;
+        [page, element] = $(this).attr('href').split("#");
 
-    originalPaddingHeight = parseFloat($('#header').css('padding-bottom'));
+        if( page == "")
+        {
+            event.preventDefault();
+            scroll_to_link($("#" + element).offset().top);
+        }
+    });
+
     window.onscroll = scroll_header;
 
     position_footer();
     window.onresize = position_footer;
-});
 
-/* Fade in different elements on the page */
-function fade_in_elements(elements)
-{
-    $(this).scrollTop(0);
-    if( elements.length )
+    /* Adjust page Y offset if necessary */
+    if( window.pageYOffset > 0 && window.pageYOffset + window.innerHeight < $(document).height() )
     {
-        $(elements[0]).animate({opacity: 1}, 500, function() {
-            fade_in_elements(elements.splice(1))
-        });
-    }
+        var scrollLocation = window.pageYOffset;
 
-    scroll_header();
-}
-
-/* Shrink or Grow header and fade background image based on location on page */
-function scroll_header()
-{
-    var scrollAmount = window.pageYOffset;
-    var homeHeight = $('#header').outerHeight();
-    if( originalPaddingHeight >= scrollAmount )
-    {
-        /* Shrink header */
-        $('#header').css('padding-bottom', originalPaddingHeight - scrollAmount);
-
-        /* Fade out header background image */
-        var proportionScrolled = scrollAmount / originalPaddingHeight;
-        var opacity = originalHomeColorOpacity + (1 - originalHomeColorOpacity) * proportionScrolled;
-        $('#header').css('background', 'linear-gradient(to right, rgba(107, 77, 168, ' + String(opacity) + '),\
-                          rgba(17, 138, 188, ' + String(opacity) + ')), url(img/higgins.jpg) no-repeat center bottom')
-        $('#header').css('background-size', 'cover');
-    }
-}
-
-/* Scroll to a link on the page */
-function scroll_to_link(event, scrollLocation)
-{
-    event.preventDefault();
-    if( scrollLocation > 0 )
-    {
         /* Shift down for navbar */
         scrollLocation -= $(".navbar").height();
 
         /* Shift down for shrinking header */
         scrollLocation -= parseFloat($('#header').css('padding-bottom'));
 
-        /* round up */
-        scrollLocation += 0.5;
+        $(this).scrollTop( scrollLocation );
+        console.log(scrollLocation);
+    }
+    scroll_header();
+});
+
+/* Fade in different elements on the page */
+function fade_in_elements(elements)
+{
+    if( elements.length )
+    {
+        $(elements[0]).animate({opacity: 1}, 500, function() {
+            fade_in_elements(elements.splice(1))
+        });
+    }
+}
+
+/* Shrink or Grow header and fade background image based on location on page */
+function scroll_header()
+{
+    var scrollAmount = window.pageYOffset;
+    if( scrollAmount > originalPaddingHeight )
+    {
+        scrollAmount = originalPaddingHeight;
+    }
+    /* Shrink header */
+    $('#header').css('padding-bottom', originalPaddingHeight - scrollAmount);
+
+    /* Fade out header background image */
+    var proportionScrolled = scrollAmount / originalPaddingHeight;
+    var opacity = originalHomeColorOpacity + (1 - originalHomeColorOpacity) * proportionScrolled;
+    $('#header').css('background', 'linear-gradient(to right, rgba(107, 77, 168, ' + String(opacity) + '),\
+                      rgba(17, 138, 188, ' + String(opacity) + ')), url(img/higgins.jpg) no-repeat center bottom')
+    $('#header').css('background-size', 'cover');
+}
+
+/* Scroll to a link on the page */
+function scroll_to_link(scrollLocation)
+{
+    if( scrollLocation > 0 )
+    {
+        /* Shift down for navbar */
+        scrollLocation -= $(".navbar").height();
+
+        /* Shift up for shrinking header */
+        scrollLocation -= parseFloat($('#header').css('padding-bottom'));
     }
     $('html, body').animate({ scrollTop: scrollLocation }, 500);
 }
 
+/* If a page is short, move footer to absolute positioning on bottom, otherwise, normal positioning */
 function position_footer()
 {
     $('footer').removeClass('short-page-footer');
